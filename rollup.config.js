654f55f311c0e0b55b0d5c955ca8f7424155e9f1
Susan Cheng <susan.doggie@gmail.com>
2022-09-16 21:26:15 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,92 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
+
+const packageJson = require('./package.json');
+
+const rollupPlugins = [
+    typescript(),
+    babel({
+        babelrc: false,
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+    }),
+    commonjs(),
+];
+
+const rollupConfig = {
+    input: 'src/index',
+    external: [
+        /node_modules/
+    ],
+};
+
+export default [
+    {
+        ...rollupConfig,
+        output: [
+            {
+              file: packageJson.main + '.js',
+              format: 'cjs',
+              sourcemap: true,
+            },
+            {
+              file: packageJson.module + '.js',
+              format: 'esm',
+              sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve({
+                extensions: ['.ts', '.tsx', '.mjs', '.js']
+            }),
+            ...rollupPlugins
+        ],
+    },
+    {
+        ...rollupConfig,
+        output: [
+            {
+                file: packageJson.main + '.web.js',
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+              file: packageJson.module + '.web.js',
+              format: 'esm',
+              sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve({
+                extensions: [
+                  '.web.ts', '.web.tsx', '.web.mjs', '.web.js',
+                  '.ts', '.tsx', '.mjs', '.js',
+                ]
+            }),
+            ...rollupPlugins
+        ],
+    },
+    {
+        input: 'src/index',
+        output: [
+            {
+                file: 'dist/index.d.ts',
+                format: 'es',
+            },
+        ],
+        plugins: [dts()],
+    },
+    {
+        input: 'src/index.web',
+        output: [
+            {
+                file: 'dist/index.web.d.ts',
+                format: 'es',
+            },
+        ],
+        plugins: [dts()],
+    },
+];
