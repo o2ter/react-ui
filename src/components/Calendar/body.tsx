97c6f4f72354b9d68612aaf21a37fa0f9fcad469
Susan Cheng <susan.doggie@gmail.com>
2022-09-23 15:29:26 +0800

    test

---
+++ src/components/Calendar/body.tsx
@@ -0,0 +1,117 @@
+//
+//  body.tsx
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2022 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import _ from 'lodash';
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { Svg, Circle } from 'react-native-svg';
+import { useTheme } from '../../theme';
+import { DateTime } from 'luxon';
+import uuid from 'react-native-uuid';
+
+import { _Date, dateToString } from './date';
+
+const style = StyleSheet.create({
+  weekContainer: {
+    width: '100%',
+    aspectRatio: 7,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  weekdayContainer: {
+    flex: 1,
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  weekdays: {
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  disabledWeekdays: {
+    color: '#adb5bd',
+  },
+})
+
+export const CalendarBody: React.FC<{
+  selected?: _Date;
+  month: number;
+  year: number;
+  selectable: (date: string) => boolean;
+  onSelect: (date: { year: number; month: number; day: number; }) => void;
+}> = ({
+  selected,
+  month,
+  year,
+  selectable,
+  onSelect,
+}) => {
+
+  const id = React.useMemo(() => uuid.v4(), []);
+
+  const theme = useTheme();
+
+  const monthObj = DateTime.fromObject({ year, month, day: 1 } as any);
+  const daysInMonth = monthObj.daysInMonth;
+  const weekdayStart = monthObj.weekday % 7;
+
+  const _selected = selected?.year === year && selected?.month === month;
+
+  const rows = [];
+  let current_row = [];
+  for (const day of _.range(1, daysInMonth + 1)) {
+    if (day == 1 && weekdayStart > 0) {
+      current_row.push(<View key={`${id}-padding-start`} style={{ flex: weekdayStart }} />);
+    }
+    if (!_.isEmpty(current_row) && (day + weekdayStart - 1) % 7 == 0) {
+      rows.push(<View key={`${id}-row-${rows.length}`} style={style.weekContainer}>{current_row}</View>);
+      current_row = [];
+    }
+    current_row.push(
+      <Pressable
+      key={`${id}-day-${day}`}
+      style={style.weekdayContainer}
+      onPress={() => { if (selectable(dateToString(year, month, day))) onSelect({ year, month, day }) }}>
+        {_selected && selected?.day === day && (
+          <Svg viewBox='0 0 100 100' style={StyleSheet.absoluteFill}>
+            <Circle cx={50} cy={50} r={40} fill={theme.colors.primary} />
+          </Svg>
+        )}
+        <Text style={[
+          style.weekdays,
+          selectable(dateToString(year, month, day)) ? {} : style.disabledWeekdays,
+          _selected && selected?.day === day ? { color: theme.colorContrast(theme.colors.primary) } : {},
+        ]}>{day}</Text>
+      </Pressable>
+    );
+  }
+  if ((daysInMonth + weekdayStart) % 7 != 0) {
+    current_row.push(<View key={`${id}-padding-end`} style={{ flex: 7 - (daysInMonth + weekdayStart) % 7 }} />);
+  }
+  rows.push(<View key={`${id}-row-${rows.length}`} style={style.weekContainer}>{current_row}</View>);
+
+  return rows;
+};
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,13 +26,29 @@
 import _ from 'lodash';
 import React from 'react';
 import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
-import { Svg, Circle } from 'react-native-svg';
-import RNPickerSelect from 'react-native-picker-select';
-import { useTheme } from '../../../hooks/useTheme';
-import { DateTime as dt } from 'luxon';
-import uuid from 'react-native-uuid';
+import PickerSelect from 'react-native-picker-select';
+import { useTheme } from '../../theme';
+import { DateTime } from 'luxon';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { _Date, dateToString } from './date';
+import { CalendarBody } from './body';
+
+import { MaterialCommunityIcons as Icon } from '../Icons';
+
+const month_name = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+]
 
 const style = StyleSheet.create({
   weekContainer: {
@@ -57,82 +73,6 @@ const style = StyleSheet.create({
   },
 })
 
-const CalendarBody = ({
-  selected,
-  month,
-  year,
-  min,
-  max,
-  onSelect,
-}) => {
-
-  const id = React.useMemo(() => uuid.v4(), []);
-
-  const theme = useTheme();
-
-  const monthObj = dt.fromObject({ year, month, day: 1 });
-  const daysInMonth = monthObj.daysInMonth;
-  const weekdayStart = monthObj.weekday % 7;
-
-  const _selected = selected?.year === year && selected?.month === month;
-
-  function selectable(date) {
-    if (min && lessThan(date, min)) return false;
-    if (max && lessThan(max, date)) return false;
-    return true;
-  }
-
-  const rows = [];
-  let current_row = [];
-  for (const day of _.range(1, daysInMonth + 1)) {
-    if (day == 1 && weekdayStart > 0) {
-      current_row.push(<View key={`${id}-padding-start`} style={{ flex: weekdayStart }} />);
-    }
-    if (!_.isEmpty(current_row) && (day + weekdayStart - 1) % 7 == 0) {
-      rows.push(<View key={`${id}-row-${rows.length}`} style={style.weekContainer}>{current_row}</View>);
-      current_row = [];
-    }
-    current_row.push(
-      <Pressable
-      key={`${id}-day-${day}`}
-      style={style.weekdayContainer}
-      onPress={() => { if (selectable(dateToString(year, month, day))) onSelect({ year, month, day }) }}>
-        {_selected && selected?.day === day && (
-          <Svg viewBox='0 0 100 100' style={StyleSheet.absoluteFill}>
-            <Circle cx={50} cy={50} r={40} fill={theme.primary} />
-          </Svg>
-        )}
-        <Text style={[
-          style.weekdays,
-          selectable(dateToString(year, month, day)) ? {} : style.disabledWeekdays,
-          _selected && selected?.day === day ? { color: theme.colorContrast(theme.primary) } : {},
-        ]}>{day}</Text>
-      </Pressable>
-    );
-  }
-  if ((daysInMonth + weekdayStart) % 7 != 0) {
-    current_row.push(<View key={`${id}-padding-end`} style={{ flex: 7 - (daysInMonth + weekdayStart) % 7 }} />);
-  }
-  rows.push(<View key={`${id}-row-${rows.length}`} style={style.weekContainer}>{current_row}</View>);
-
-  return rows;
-};
-
-const month_name = [
-  'january',
-  'february',
-  'march',
-  'april',
-  'may',
-  'june',
-  'july',
-  'august',
-  'september',
-  'october',
-  'november',
-  'december',
-]
-
 const CalendarBase = ({
   value,
   min,
@@ -141,22 +81,28 @@ const CalendarBase = ({
 }) => {
 
   const [current, setCurrent] = React.useState(() => {
-    const now = dt.now();
+    const now = DateTime.now();
     return { month: value?.month ?? now.month, year: value?.year ?? now.year }
   });
 
-  const [yearText, setYearText] = React.useState(null);
+  const [yearText, setYearText] = React.useState<string | null>(null);
 
   function submit() {
 
     try {
 
-      const year = parseInt(yearText);
+      const year = parseInt(yearText ?? '');
       if (year >= 1970 && year < 9999) setCurrent(current => ({ ...current, year }));
 
     } catch {}
 
     setYearText(null);
+  }
+
+  function selectable(date: string) {
+    if (min && _Date.lessThan(date, min)) return false;
+    if (max && _Date.lessThan(max, date)) return false;
+    return true;
   }
 
   return (
@@ -167,7 +113,7 @@ const CalendarBase = ({
     }}>
       <View style={{ padding: 8, flexDirection: 'row', justifyContent: 'space-between' }}>
         <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
-          <RNPickerSelect
+          <PickerSelect
           value={month_name[current.month - 1]}
           items={month_name.map(x => ({ label: _.upperFirst(x), value: x }))}
           placeholder={{}}
@@ -212,7 +158,7 @@ const CalendarBase = ({
         <Text style={style.weekdays}>FR</Text>
         <Text style={style.weekdays}>SA</Text>
       </View>
-      <CalendarBody selected={value} min={min} max={max} onSelect={({ year, month, day }) => {
+      <CalendarBody selected={value} selectable={selectable} onSelect={({ year, month, day }) => {
         onChange(dateToString(year, month, day));
       }} {...current} />
     </View>
