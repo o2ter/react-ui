648e0c245641f7a1d70019793db80d5d8b002b2a
Susan Cheng <susan.doggie@gmail.com>
2022-09-30 23:15:51 +0800

    Update Form.stories.js

--- stories/Form.stories.js
+++ stories/Form.stories.js
@@ -10,18 +10,21 @@ export default {
   component: Form,
 };
 
-export const TextInput = () => <Form 
-schema={{
-  email: string().email().required(),
-  name: object({
-    first: string().required(),
-    last: string().required(),
-  }),
-}}>
+export const TextInput = () => <Form
+  schema={{
+    email: string().email().required(),
+    name: object({
+      first: string().notEmpty(),
+      last: string().notEmpty(),
+    }),
+  }}>
   <Form.TextField name='email' />
+  <Form.ErrorMessage name='email' />
   <Form.Group name='name'>
     <Form.TextField name='first' />
+    <Form.ErrorMessage name='first' />
     <Form.TextField name='last' />
+    <Form.ErrorMessage name='last' />
   </Form.Group>
 </Form>;
 TextInput.args = {
