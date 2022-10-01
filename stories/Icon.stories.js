98538c57b4732dc727d73f2326a9702b418a12f2
Susan Cheng <susan.doggie@gmail.com>
2022-10-01 22:41:09 +0800

    Create Icon.stories.js

---
+++ stories/Icon.stories.js
@@ -0,0 +1,12 @@
+
+import _ from 'lodash';
+import React from 'react';
+
+import { Icon } from '../src/components/Icon';
+
+export default {
+  title: 'Icon',
+  component: Icon,
+};
+
+export const Default = (props) => <Icon {...props} />;
