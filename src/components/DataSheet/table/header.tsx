68fb63571c6fab60893433683ab3ed3164cc159d
Susan Cheng <susan.doggie@gmail.com>
2023-09-01 14:06:01 +0800

    Update header.tsx

--- src/components/DataSheet/table/header.tsx
+++ src/components/DataSheet/table/header.tsx
@@ -67,6 +67,7 @@ export const DataSheetHeader = <T extends object>({
             border: 1,
             borderStyle: 'solid',
             borderColor: '#DDD',
+            backgroundColor: '#F6F8FF',
             borderBottomStyle: isRowSelected(0) ? 'double' : 'solid',
             borderBottomColor: isRowSelected(0) ? '#2185D0' : '#DDD',
           }, stickyRowNumberStyle])} />
@@ -86,10 +87,8 @@ export const DataSheetHeader = <T extends object>({
                 borderLeft: 0,
                 borderStyle: 'solid',
                 borderColor: '#DDD',
-                backgroundColor: '#F6F8FF',
                 borderBottomStyle: isRowSelected(0) || isCellSelected(0, col) ? 'double' : 'solid',
                 borderBottomColor: isRowSelected(0) || isCellSelected(0, col) ? '#2185D0' : '#DDD',
-                zIndex: 2,
               }, headerItemContainerStyle])} />
           )}
         />
