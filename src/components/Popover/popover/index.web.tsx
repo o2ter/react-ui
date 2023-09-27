94d7aa0f3891c0ab0078687669155d35134a51d7
Susan Cheng <susan.doggie@gmail.com>
2023-09-28 00:18:11 +0800

    test

---
+++ src/components/Popover/popover/index.web.tsx
@@ -0,0 +1,53 @@
+//
+//  index.web.tsx
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2023 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import _ from 'lodash';
+import React from 'react';
+import { createMemoComponent } from '../../../internals/utils';
+import View from '../../View';
+import { useMergeRefs } from 'sugax';
+import { useWindowEvent } from '../../../hooks/webHooks';
+import { LayoutRectangle } from 'react-native';
+
+type PopoverProps = {
+};
+
+export const Popover = createMemoComponent(({
+  children,
+}: React.PropsWithChildren<PopoverProps>, forwardRef: React.ForwardedRef<React.ComponentRef<typeof View>>) => {
+
+  const viewRef = React.useRef<React.ComponentRef<typeof View>>();
+  const ref = useMergeRefs(viewRef, forwardRef);
+
+  const [layout, setLayout] = React.useState<LayoutRectangle>();
+
+  useWindowEvent('scroll', () => {
+    viewRef.current?.measureInWindow((x, y, width, height) => setLayout({ x, y, width, height }));
+  }, true);
+
+  return (
+    <View ref={ref}>{children}</View>
+  );
+});
